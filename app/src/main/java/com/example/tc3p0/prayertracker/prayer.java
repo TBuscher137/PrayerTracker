1316cb4d28818a8117414de1ad6141625468dbdb
TechSychotic <brandon.s.boughton@gmail.com>
2015-04-13 15:07:36 -0500

    Strings/Methods
    I changed the strings and added the methods and setters.

--- app/src/main/java/com/example/tc3p0/prayertracker/prayer.java
+++ app/src/main/java/com/example/tc3p0/prayertracker/prayer.java
@@ -9,111 +9,91 @@ import java.util.jar.Attributes;
 public class prayer {
     public static void main(String[] args) {
 
-        public String title() {
-
-        String name = "title";
-        int len = title.length();
-        char[] tmpCharArray = new char[len];
-        char[] charArray = new char[len];
-
-        for (int i = 0; i < len; i++);
-            tmpCharArray[i] = name.charAt(i);
-
-        return "title";
+        boolean answeredPrayer = true;
+        if(answeredPrayer == true){
+            System.out.print(true);
+        }else{
+            System.out.print(false);
         }
 
 
-        public String Description() {
+        String title[] = new String[1];
+        title[0] = " ";
 
-            String name = "Description";
-            int len = Description().length();
-            char[] tmpCharArray = new char[len];
-            char[] charArray = new char[len];
+        String description[] = new String[1];
+        description[0] = "";
 
-            for (int i = 0; i < len; i++);
-            tmpCharArray[i] = name.charAt(i);
+        String photo[] = new String[1];
+        photo[0] = " ";
 
 
-            return "description";
+        String name[] = new String[1];
+        name[0] = " ";
+
+        String phoneNumber[] = new String[1];
+        phoneNumber[0] = " ";
+
+        String email[] = new String[1];
+        email[0] = " ";
+
+        {
+            GregorianCalendar date = new GregorianCalendar();
+
+            int year = date.get(GregorianCalendar.YEAR);
+            int month = date.get(GregorianCalendar.MONTH);
+            int day = date.get(GregorianCalendar.DAY_OF_MONTH);
+
+            date.setTimeInMillis(1234567898765L);
 
 
         }
 
-    public String Photo() {
-        String name = "Photo";
-        int len = Photo().length();
-        char[] tmpCharArray = new char[len];
-        char[] charArray = new char[len];
-
-        for (int i = 0; i < len; i++);
-        tmpCharArray[i] = name.charAt(i);
-
-        return String.format("Photo ");
+    public String getTitle() {
+        return title;
     }
 
-
-
-
-
-    public String Name() {
-        String name = "Name";
-        int len = Name().length();
-        char[] tmpCharArray = new char[len];
-        char[] charArray = new char[len];
-
-        for (int i = 0; i < len; i++);
-        tmpCharArray[i] = name.charAt(i);
-
-
-        return "Name" + " ";
+    public void setTitle(String prayerTitle) {
+        title = prayerTitle;
     }
 
-    public String phoneNumber() {
-        String name = "phoneNumber";
-        int len = phoneNumber().length();
-        char[] tmpCharArray = new char[len];
-        char[] charArray = new char[len];
-
-        for (int i = 0; i < len; i++);
-        tmpCharArray[i] = name.charAt(i);
-
-
-        return "Phone Number" + " ";
+    public String getDescription() {
+        return description;
     }
 
-    public String email() {
-        String name = "email";
-        int len = email().length();
-        char[] tmpCharArray = new char[len];
-        char[] charArray = new char[len];
-
-        for (int i = 0; i < len; i++);
-        tmpCharArray[i] = name.charAt(i);
-
-
-        return "email" + " ";
-
-    }
-public GregorianCalendar
-
-    {
-        GregorianCalendar date = new GregorianCalendar();
-        int year = date.get(GregorianCalendar.YEAR);
-        int month = date.get(GregorianCalendar.MONTH);
-        int day = date.get(GregorianCalendar.DAY_OF_MONTH);
-
-        date.setTimeInMillis(1234567898765L);
-
-
+    public void setDescription(String prayerDescription) {
+        description = prayerDescription;
     }
 
-  public getName{
-      return Name();
-
+    public String getPhoto() {
+        return photo;
     }
 
+    public void setPhoto(String prayerPhoto) {
+        photo = prayerPhoto;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String personName) {
+        name = personName;
+    }
+
+    public String getPhoneNumber() {
+        return getPhoneNumber;
+    }
+
+    public void setPhoneNumber(String personPhoneNumber) {
+        phoneNumber = personPhoneNumber;
+    }
+
+    public String getEmail() {
+        return getEmail;
+    }
+
+    public void setEmail(String personEmail) {
+        email = personEmail;
     }
 }
-
-
 }
