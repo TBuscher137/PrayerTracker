505b311e93ff4ba20c9d35873f7edc651b92a243
TechSychotic <brandon.s.boughton@gmail.com>
2015-04-13 00:08:11 -0500

    Sunday

--- app/src/main/java/com/example/tc3p0/prayertracker/prayer.java
+++ app/src/main/java/com/example/tc3p0/prayertracker/prayer.java
@@ -1,56 +1,119 @@
 
-import java.time.Year;
 import java.util.*;
+import java.util.jar.Attributes;
 
 /**
- *
  * @author Brandon Boughton
  */
+
 public class prayer {
+    public static void main(String[] args) {
 
-    
-    public String Title() {
-        return "title" + " ";
-    }
+        public String title() {
 
-    public String Description(){
-            return "description" + " ";
+        String name = "title";
+        int len = title.length();
+        char[] tmpCharArray = new char[len];
+        char[] charArray = new char[len];
 
-    }
+        for (int i = 0; i < len; i++);
+            tmpCharArray[i] = name.charAt(i);
+
+        return "title";
+        }
+
+
+        public String Description() {
+
+            String name = "Description";
+            int len = Description().length();
+            char[] tmpCharArray = new char[len];
+            char[] charArray = new char[len];
+
+            for (int i = 0; i < len; i++);
+            tmpCharArray[i] = name.charAt(i);
+
+
+            return "description";
+
+
+        }
 
     public String Photo() {
+        String name = "Photo";
+        int len = Photo().length();
+        char[] tmpCharArray = new char[len];
+        char[] charArray = new char[len];
+
+        for (int i = 0; i < len; i++);
+        tmpCharArray[i] = name.charAt(i);
+
         return String.format("Photo ");
     }
 
+
+
+
+
     public String Name() {
+        String name = "Name";
+        int len = Name().length();
+        char[] tmpCharArray = new char[len];
+        char[] charArray = new char[len];
+
+        for (int i = 0; i < len; i++);
+        tmpCharArray[i] = name.charAt(i);
+
+
         return "Name" + " ";
     }
 
-
     public String phoneNumber() {
+        String name = "phoneNumber";
+        int len = phoneNumber().length();
+        char[] tmpCharArray = new char[len];
+        char[] charArray = new char[len];
+
+        for (int i = 0; i < len; i++);
+        tmpCharArray[i] = name.charAt(i);
+
+
         return "Phone Number" + " ";
     }
 
-
     public String email() {
+        String name = "email";
+        int len = email().length();
+        char[] tmpCharArray = new char[len];
+        char[] charArray = new char[len];
+
+        for (int i = 0; i < len; i++);
+        tmpCharArray[i] = name.charAt(i);
+
+
         return "email" + " ";
 
     }
+public GregorianCalendar
 
-
-
-
-    public static void main(String[] args) {
-       GregorianCalendar date = new GregorianCalendar();
-
-        int year0 = date.get(GregorianCalendar.YEAR);
-        int month0 = date.get(GregorianCalendar.MONTH);
-        int day0 = date.get(GregorianCalendar.DAY_OF_MONTH);
+    {
+        GregorianCalendar date = new GregorianCalendar();
+        int year = date.get(GregorianCalendar.YEAR);
+        int month = date.get(GregorianCalendar.MONTH);
+        int day = date.get(GregorianCalendar.DAY_OF_MONTH);
 
         date.setTimeInMillis(1234567898765L);
 
-        int year1
 
     }
-    
+
+  public getName{
+      return Name();
+
+    }
+
+    }
+}
+
+
 }
